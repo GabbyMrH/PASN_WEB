c998503070572e96e0477d84e9c0d856fc8301a2
GabbyMrH <932585440@qq.com>
2019-12-17 15:10:41 +0800

    初始化版本

---
+++ WEB_API/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+$router->get('/', function () use ($router) {
+    //return $router->app->version();
+    $test = Crypt::encrypt('123456');
+    $data = DB::table('ed_booking')->orderByDesc('create_date')->get();
+    return response()->json($data);
+});
