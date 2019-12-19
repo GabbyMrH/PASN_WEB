5e35e20d9a6a83d279da69ceff524be0103f5349
GabbyMrH <932585440@qq.com>
2019-12-19 16:13:22 +0800

    增加artisan命令

--- WEB_API/bootstrap/app.php
+++ WEB_API/bootstrap/app.php
@@ -79,6 +79,8 @@ $app->routeMiddleware([
 $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+//开启全部artisan功能
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
