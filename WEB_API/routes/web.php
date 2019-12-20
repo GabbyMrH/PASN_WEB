e438976f6383e07aa0c1b9272d752d0503aa1e88
GabbyMrH <932585440@qq.com>
2019-12-20 12:02:40 +0800

    路由增加版本前缀

--- WEB_API/routes/web.php
+++ WEB_API/routes/web.php
@@ -11,15 +11,27 @@
 |
 */
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
-$router->get('/', function () use ($router) {
-    //return $router->app->version();
-    $test = Crypt::encrypt('123456');
-    $data = DB::table('ed_booking')->orderByDesc('create_date')->get();
-    return response()->json($data);
+// $router->get('/', function () use ($router) {
+//     //return $router->app->version();
+//     // $test = Crypt::encrypt('123456');
+//     $data = DB::table('users')->where('id','2')->update(['created_at'=>Carbon::now(),'updated_at'=>Carbon::now()]);
+//     // return response()->json($data);
+// });
+$router->group(['prefix' => 'v1'], function ($router) {
+    //获取token
+    $router->post('user', 'AuthController@store');
+    //刷新token
+    $router->put('user/current','AuthController@update');
+    //删除token
+    $router->delete('user/current','AuthController@destroy');
+    
+    //获取用户信息
+    $router->group(['middleware' => 'auth:api'], function ($router) {
+        $router->get('user', 'UsersController@info');
+    });
 });
-//测试jwt
-$router->get('login','AuthController@store');
