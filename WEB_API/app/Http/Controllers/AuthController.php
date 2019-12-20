95edcf27c5e33da0a4e0bd49064fba1af901a8a5
GabbyMrH <932585440@qq.com>
2019-12-20 12:03:16 +0800

    优化token获取方式

--- WEB_API/app/Http/Controllers/AuthController.php
+++ WEB_API/app/Http/Controllers/AuthController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -11,31 +13,38 @@ use Tymon\JWTAuth\JWTAuth;
 
 class AuthController extends Controller
 {
-    protected $jwt;
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(JWTAuth $jwt)
+    public function __construct(User $user)
     {
-        //注入jwt
-        $this->jwt = $jwt;
+
     }
 
     //登录验证
     public function store(Request $request)
     {
-        $data_check = Validator::make($request->all(),[
-            'customer_id'=>'required',
-            'password'=>'required'
+        $data_check = Validator::make($request->all(), [
+            'username' => 'required',
+            'password' => 'required'
         ]);
-        if($data_check->fails()){
-            return response()->json(StatusController::error());
+
+        if ($data_check->fails()) {
+            return response()->json(StatusController::paramError());
         }
-        //DB::table('ed_customer')->where('customer_id','LT')->update(['password'=>Hash::make('123456')]);
-        //密码加密为Hash:make,验证为Hash::check(值)
-        //$test = Hash::check('123456', '$2y$10$MtgCl5oe9Kllec7lborxiuyE7U.chMfFQWUlP1G9KshfH5xAu0FF.');
+        //验证数据表数据
+        $token = Auth::guard('api')->attempt([
+            'username' => $request->username,
+            'password' => $request->password
+        ]);
+        if (!$token) {
+            return response()->json(StatusController::dataError());
+        }
+
+        //返回jwt信息
+        return $this->userToken($token)->setStatusCode(201);
 
     }
 
@@ -43,9 +52,23 @@ class AuthController extends Controller
     public function userToken($token)
     {
         return response()->json([
-            'access_token'=>$token,
-            'token_type'=>'Bearer',
-            'expires_in'=>Auth::guard('api')->factory()->getTTL() * 60
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => Auth::guard('api')->factory()->getTTL() * 60,
+            'code'=>2001
         ]);
+    }
+
+    /* 刷新token */
+    public function update()
+    {
+        $token = Auth::guard('api')->refresh();
+        return $this->userToken($token);
+    }
+    /* 删除token */
+    public function destroy()
+    {
+        Auth::guard('api')->logout();
+        return response(null, 204);
     }
 }
