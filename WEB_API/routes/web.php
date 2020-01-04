6bacf2fb959082058621ff4c15f1ceafe07d1a9e
GabbyMrH <932585440@qq.com>
2020-01-04 20:22:21 +0800

    库存余额总量后台路由

--- WEB_API/routes/web.php
+++ WEB_API/routes/web.php
@@ -16,12 +16,7 @@ use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
-// $router->get('/', function () use ($router) {
-//     //return $router->app->version();
-//     // $test = Crypt::encrypt('123456');
-//     $data = DB::table('users')->where('id','2')->update(['created_at'=>Carbon::now(),'updated_at'=>Carbon::now()]);
-//     // return response()->json($data);
-// });
+
 $router->group(['prefix' => 'v1'], function ($router) {
     //获取token
     $router->post('user', 'AuthController@store');
@@ -33,8 +28,13 @@ $router->group(['prefix' => 'v1'], function ($router) {
     //获取用户信息 路由组调用auth中间件,中间件会触发illuminate/auth/AuthManage.php下的guard,-
     //-该函数会调用getDefaultDriver函数,接下来会查询config目录下「 lumen默认的Auth.php内的defaults.guard 」-
     //-接下来，该guard的配置就在该文件内找到了，就是默认的api guard，当前的服务提供者为users模型
+
     $router->group(['middleware' => 'auth:api'], function ($router) {
         $router->get('user', 'UsersController@info');
+
+        /* 面板管理 */
+        //库存余额总-量
+        $router->get('inventoryTotal','PanelController@inventoryTotal');
 
         /* 订单管理 */
         //库存余额
