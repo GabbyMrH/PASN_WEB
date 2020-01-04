600036ba7e81eef017b94fec44db4c47ed871324
GabbyMrH <932585440@qq.com>
2020-01-05 01:31:29 +0800

    修正注释

--- WEB_API/app/Http/Controllers/InventoryController.php
+++ WEB_API/app/Http/Controllers/InventoryController.php
@@ -11,14 +11,14 @@ class InventoryController extends Controller
     /**
     * list列表
     * @param page_limit(required)
-    * */
+    */
    public function list(Request $request,EdInventory $edInventory)
    {
        $req_data = $request->all();
        if(!$req_data){
            return response()->json(StatusController::paramError($req_data));
        }
-       //过滤传递参数并交由模型处理数据
+       //过滤传递参数
        $results = $edInventory->dataList(array_filter($req_data));
        return response()->json(StatusController::success($results));
 
