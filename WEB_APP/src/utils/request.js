21a4e04c3eeb007f84601f0832e6fd4ce6aeb148
GabbyMrH <932585440@qq.com>
2020-02-25 12:13:37 +0800

    引入文件更改

--- WEB_APP/src/utils/request.js
+++ WEB_APP/src/utils/request.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { Message } from 'element-ui'
+import { Message, MessageBox } from 'element-ui'
 import store from '@/store'
 import { getToken } from '@/utils/auth'
 
@@ -50,25 +50,25 @@ service.interceptors.response.use(
     // if the custom code is not 20000, it is judged as an error.
     if (res.code !== 2001) {
       Message({
-        message: res.message || '错误，请稍后重试',
+        message: res.msg || '错误，请稍后重试',
         type: 'error',
         duration: 5 * 1000
       })
 
-      // 50008: Illegal token; 50012: Other clients logged in; 50014: Token expired;
-      // if (res.code === 50008 || res.code === 50012 || res.code === 50014) {
-      //   // to re-login
-      //   MessageBox.confirm('You have been logged out, you can cancel to stay on this page, or log in again', 'Confirm logout', {
-      //     confirmButtonText: 'Re-Login',
-      //     cancelButtonText: 'Cancel',
-      //     type: 'warning'
-      //   }).then(() => {
-      //     store.dispatch('user/resetToken').then(() => {
-      //       location.reload()
-      //     })
-      //   })
-      // }
-      // return Promise.reject(new Error(res.message || 'Error'))
+      // 50008: 非法token; 50012: 其他用户登录; 50014: Token过期;
+      if (res.code === 50008 || res.code === 50012 || res.code === 4005) {
+        // to re-login
+        MessageBox.confirm('您的登录已过期, 您可以取消并停留在该页面, 或重新登录', '登录过期', {
+          confirmButtonText: '重新登录',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          store.dispatch('user/resetToken').then(() => {
+            location.reload()
+          })
+        })
+      }
+      return Promise.reject(new Error(res.msg || 'Error'))
     } else {
       return res
     }
