cd3d9c871f2bf3d4333f76a9b572cea11efe8911
GabbyMrH <932585440@qq.com>
2020-02-26 13:17:34 +0800

    ⚙️余额控制器测试用户数据

--- WEB_API/app/Http/Controllers/InventoryController.php
+++ WEB_API/app/Http/Controllers/InventoryController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\EdInventory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 
 class InventoryController extends Controller
 {
     /**
-    * list列表
-    * @param page_limit(required)
-    */
+     * @param Request $request
+     * @param EdInventory $edInventory
+     * @return \Illuminate\Http\JsonResponse
+     */
    public function list(Request $request,EdInventory $edInventory)
    {
        $req_data = $request->all();
@@ -22,5 +24,10 @@ class InventoryController extends Controller
        $results = $edInventory->dataList(array_filter($req_data));
        return response()->json(StatusController::success($results));
 
+   }
+
+   public function test()
+   {
+       return response()->json(Auth::user()->ed_customer_customer_id);
    }
 }
