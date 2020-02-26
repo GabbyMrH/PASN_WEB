0c56d6b5eba3096cc7f807e6b3f20b891946f186
GabbyMrH <932585440@qq.com>
2020-02-26 22:17:00 +0800

    控制面板API接口js参数变更

--- WEB_APP/src/api/panel.js
+++ WEB_APP/src/api/panel.js
@@ -3,7 +3,8 @@ import request from '@/utils/request'
 // 余额总量
 export function inventoryTotal() {
   return request({
-    url: 'inventoryTotal',
-    method: 'get'
+    url: 'inventory',
+    method: 'get',
+    params: { page: 1, page_limit: 20 }
   })
 }
