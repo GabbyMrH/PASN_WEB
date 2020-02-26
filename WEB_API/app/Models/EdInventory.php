dd6c828317864b41c5a39427b21cd64debf1c875
GabbyMrH <932585440@qq.com>
2020-02-26 22:15:43 +0800

    优化库存余额根据账号类型显示对应数据

--- WEB_API/app/Models/EdInventory.php
+++ WEB_API/app/Models/EdInventory.php
@@ -35,34 +35,40 @@ class EdInventory extends Model
 
     public static $customer_id;
 
-
     public function __construct()
     {
         self::$customer_id = Auth::user()->ed_customer_customer_id;
-        if(!self::$customer_id){
+        if (!self::$customer_id) {
             false;
         }
     }
 
     //数据处理
-    public function dataList($req_data)
+    public function dataList($requestData)
     {
         //过滤page和page_limit筛选条件
-        $filter_condition = Arr::except($req_data, ['page', 'page_limit']);
-            // 返回最高级别所有数据
-            if(self::$customer_id == CustomerType::admin){
-                return self::where($filter_condition)->orderBy('create_date', 'DESC')->paginate($req_data['page_limit']);
+        $filter_condition = Arr::has($requestData,['page', 'page_limit']) ? Arr::except($requestData, ['page', 'page_limit']) : '';
+
+        //过滤变量不为空表示有其他参数传入则以该数据做筛选条件
+        if($filter_condition){
+            // 返回管理员级别所有数据
+            if (self::$customer_id == CustomerType::admin) {
+                return self::where($filter_condition)->orderBy('create_date', 'DESC')->paginate($requestData['page_limit']);
             }
-                //默认值则判断传递参数返回对应数据
-                return self::where($filter_condition)
-                    ->where('customer_id',self::$customer_id)
-                    ->orderBy('create_date', 'DESC')
-                    ->paginate($req_data['page_limit']);
+            //默认值则判断传递参数返回对应数据
+            return self::where($filter_condition)
+                ->where('customer_id', self::$customer_id)
+                ->orderBy('create_date', 'DESC')
+                ->paginate($requestData['page_limit']);
+        }
+        // 没有过滤条件则不需要以筛选条件传入
+        if (self::$customer_id == CustomerType::admin) {
+            return $this->orderBy('create_date', 'DESC')->paginate($requestData['page_limit']);
+        }
+        //默认值则判断传递参数返回对应数据
+        return $this->where('customer_id', self::$customer_id)
+            ->orderBy('create_date', 'DESC')
+            ->paginate($requestData['page_limit']);
     }
 
-    //dashboard 总量
-    public function total()
-    {
-        return $this->count();
-    }
 }
