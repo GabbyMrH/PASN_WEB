3e94c134506b48c82df05d8de3cd7ab08fe05406
GabbyMrH <932585440@qq.com>
2020-02-26 13:16:07 +0800

    余额模型封装查询客户类型

--- WEB_API/app/Models/EdInventory.php
+++ WEB_API/app/Models/EdInventory.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use App\Enums\CustomerType;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Auth;
 
 class EdInventory extends Model
 {
@@ -31,12 +33,31 @@ class EdInventory extends Model
      */
     protected $keyType = 'string';
 
+    public static $customer_id;
+
+
+    public function __construct()
+    {
+        self::$customer_id = Auth::user()->ed_customer_customer_id;
+        if(!self::$customer_id){
+            false;
+        }
+    }
+
     //数据处理
     public function dataList($req_data)
     {
         //过滤page和page_limit筛选条件
         $filter_condition = Arr::except($req_data, ['page', 'page_limit']);
-        return $this->where($filter_condition)->orderBy('create_date', 'DESC')->paginate($req_data['page_limit']);
+            // 返回最高级别所有数据
+            if(self::$customer_id == CustomerType::admin){
+                return self::where($filter_condition)->orderBy('create_date', 'DESC')->paginate($req_data['page_limit']);
+            }
+                //默认值则判断传递参数返回对应数据
+                return self::where($filter_condition)
+                    ->where('customer_id',self::$customer_id)
+                    ->orderBy('create_date', 'DESC')
+                    ->paginate($req_data['page_limit']);
     }
 
     //dashboard 总量
