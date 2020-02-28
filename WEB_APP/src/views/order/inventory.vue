11eac3a22681fdc6fb493ae73f25c9cf5c05dc03
GabbyMrH <932585440@qq.com>
2020-02-29 00:03:48 +0800

    库存余额优化时间显示问题

--- WEB_APP/src/views/order/inventory.vue
+++ WEB_APP/src/views/order/inventory.vue
@@ -209,7 +209,7 @@ export default {
         // Just to simulate the time of the request
         setTimeout(() => {
           this.listLoading = false
-        }, 1.5 * 1000)
+        }, 1 * 1000)
       })
     },
     handleFilter() {
