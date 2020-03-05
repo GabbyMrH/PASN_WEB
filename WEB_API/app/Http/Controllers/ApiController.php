d050351988449db1688fc0cf3ee77bc48738a220
GabbyMrH <932585440@qq.com>
2020-03-05 15:52:04 +0800

    APi控制器增加api说明

---
+++ WEB_API/app/Http/Controllers/ApiController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    /**
+     * PASN WEB API文档
+     * @return string
+     */
+    public function index()
+    {
+        return 'PASN WEB API文档';
+    }
+}
