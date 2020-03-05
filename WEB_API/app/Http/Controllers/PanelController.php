401374e06e2108c4650c0542b3b181f15a8b2ee2
GabbyMrH <932585440@qq.com>
2020-03-05 15:53:13 +0800

    Panel增加API说明

--- WEB_API/app/Http/Controllers/PanelController.php
+++ WEB_API/app/Http/Controllers/PanelController.php
@@ -7,9 +7,22 @@ use Illuminate\Http\Request;
 
 class PanelController extends Controller
 {
-    //inventory 总量
+    /**
+     * inventory total
+     * inventory 总量
+     * @param EdInventory $edInventory
+     * @return \Illuminate\Http\JsonResponse
+     * @response {
+     * "Tips":"返回分页后的入库列表，从中提取总数"
+     * }
+     */
     public function inventoryTotal(EdInventory $edInventory)
     {
-        return response()->json(StatusController::success($edInventory->dataList()));
+        // 需传array数组
+        $requestParams = [
+            'page'=>1,
+            'page_limit'=>10
+        ];
+        return response()->json(StatusController::success($edInventory->queryList($requestParams)));
     }
 }
