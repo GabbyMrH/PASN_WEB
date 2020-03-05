4567da0cc5a61fcdef025a35ad4d49282cfa4c0d
GabbyMrH <932585440@qq.com>
2020-03-05 15:54:34 +0800

    Users控制器增加API说明

--- WEB_API/app/Http/Controllers/UsersController.php
+++ WEB_API/app/Http/Controllers/UsersController.php
@@ -17,7 +17,26 @@ class UsersController extends Controller
         //
     }
 
-    //用户信息
+    /**
+     * user info
+     * 用户信息
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @response {
+     *   "data": {
+     *   "id": 2,
+     *   "name": "admin",
+     *   "username": "123456",
+     *   "created_at": "2019-12-19 15:08:23.910",
+     *  "updated_at": "2019-12-19 15:08:23.910",
+     *  "roles": "admin",
+     *  "avatar": "http://pasn.api.com/assets/avatar/avatar.jpg",
+     *  "ed_customer_customer_id": "system"
+     *  },
+     *  "code": 2001,
+     *  "msg": "操作成功"
+     *  }
+     */
     public function info(Request $request)
     {
         return response()->json(StatusController::success(Auth::user()));
