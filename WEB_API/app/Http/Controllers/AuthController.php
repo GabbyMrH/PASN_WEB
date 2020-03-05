2a00034a42cd81c0b97671b4fdc8f69e2c0ad97c
GabbyMrH <932585440@qq.com>
2020-03-05 15:52:16 +0800

    Auth增加API说明

--- WEB_API/app/Http/Controllers/AuthController.php
+++ WEB_API/app/Http/Controllers/AuthController.php
@@ -19,7 +19,20 @@ class AuthController extends Controller
 
     }
 
-    //登录验证
+    /**
+     * user login check
+     * 登录验证
+     * @param Request $request
+     * @queryParam username required 账号
+     * @queryparam password required 密码
+     * @return \Illuminate\Http\JsonResponse
+     * @response {
+     *   "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wYXNuLmFwaS5jb21cL3YxXC91c2VyIiwiaWF0IjoxNTgzMzc5MjAwLCJleHAiOjE1ODMzODY0MDAsIm5iZiI6MTU4MzM3OTIwMCwianRpIjoiYVRPdU00aW1ZejVxUWFteCIsInN1YiI6MiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.vZjnZLIYRwu_USyHCFUCCVe1J14yUTdx1t4H2SGgUiY",
+     *   "token_type": "Bearer",
+     *   "expires_in": "120",
+     *   "code": 2001
+     *   }
+     */
     public function store(Request $request)
     {
         $data_check = Validator::make($request->all(), [
@@ -44,7 +57,12 @@ class AuthController extends Controller
 
     }
 
-    //生成jwt token
+    /**
+     * generate user token
+     * 生成jwt token
+     * @param $token
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function userToken($token)
     {
         return response()->json([
@@ -56,13 +74,32 @@ class AuthController extends Controller
         ]);
     }
 
-    /* 刷新token */
+    /**
+     * refresh user token
+     * 刷新token
+     * @return \Illuminate\Http\JsonResponse
+     * @response {
+     *   "access_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9wYXNuLmFwaS5jb21cL3YxXC91c2VyIiwiaWF0IjoxNTgzMzgwOTQ3LCJleHAiOjE1ODMzODgxNDcsIm5iZiI6MTU4MzM4MDk0NywianRpIjoiZVNCcWJ2SzNQZXFscHZuNyIsInN1YiI6MiwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.APLXxUp5wWk8mpWpWLUrUA78mkiz3HPcBTqow_3gQgo",
+     *   "token_type": "Bearer",
+     *   "expires_in": "120",
+     *   "code": 2001
+     *   }
+     */
     public function update()
     {
         $token = Auth::guard('api')->refresh();
         return $this->userToken($token);
     }
-    /* 删除token */
+
+    /**
+     * delete user token
+     * 删除token
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @response {
+     * null
+     * }
+     */
     public function destroy(Request $request)
     {
         Auth::guard('api')->logout();
