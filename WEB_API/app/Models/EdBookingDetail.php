df36ccdd0758d45ecf23381949cdd29690ee30c9
GabbyMrH <932585440@qq.com>
2020-04-06 18:49:56 +0800

    增加编辑和删除功能(detail)

--- WEB_API/app/Models/EdBookingDetail.php
+++ WEB_API/app/Models/EdBookingDetail.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
 
 class EdBookingDetail extends Model
 {
@@ -24,5 +26,36 @@ class EdBookingDetail extends Model
     public function booking()
     {
         return $this->belongsTo('App\Models\EdBooking','booking_id');
+    }
+
+    // 编辑detail
+    public function queryEdit($query)
+    {
+        $filterData = Arr::except($query,['booking_id','inboundid']);
+        try {
+            $this->where([
+                'inboundid'=>$query['inboundid'],
+                'booking_id'=>$query['booking_id']
+            ])->update($filterData);
+        } catch(\Exception $e) {
+            Log::error($e->getMessage());
+            return $e->getMessage();
+        }
+        return true;
+
+    }
+    // 删除detail
+    public function queryDelete($query)
+    {
+        try {
+            $this->where([
+                'inboundid'=>$query['inboundid'],
+                'booking_id'=>$query['booking_id']
+            ])->delete();
+        } catch(\Exception $e) {
+            Log::error($e->getMessage());
+            return $e->getMessage();
+        }
+        return true;
     }
 }
