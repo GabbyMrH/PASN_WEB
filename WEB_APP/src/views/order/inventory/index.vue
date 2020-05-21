8c2baa33bc512873869a89d1985abc4cedad90a1
GabbyMrH <932585440@qq.com>
2020-05-22 00:18:28 +0800

    修改样式i

--- WEB_APP/src/views/order/inventory/index.vue
+++ WEB_APP/src/views/order/inventory/index.vue
@@ -1,69 +1,73 @@
 <template>
   <div class="app-container">
-    <div class="filter-container">
-      <el-input v-model="listQuery.warehouse_code" clearable :placeholder="$t('table.warehouse_code')" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-      <el-input v-model="listQuery.sku_no" clearable :placeholder="$t('table.sku_no')" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-      <el-input v-model="listQuery.po_no" clearable :placeholder="$t('table.po_no')" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
-        {{ $t('table.search') }}
-      </el-button>
-      <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">
-        {{ $t('table.export') }}
-      </el-button>
-    </div>
+    <el-card class="box-card">
+      <div class="filter-container">
+        <el-input v-model="listQuery.warehouse_code" clearable :placeholder="$t('table.warehouse_code')" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+        <el-input v-model="listQuery.sku_no" clearable :placeholder="$t('table.sku_no')" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+        <el-input v-model="listQuery.po_no" clearable :placeholder="$t('table.po_no')" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+        <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+          {{ $t('table.search') }}
+        </el-button>
+        <el-button v-waves :loading="downloadLoading" class="filter-item" type="primary" icon="el-icon-download" @click="handleDownload">
+          {{ $t('table.export') }}
+        </el-button>
+      </div>
+    </el-card>
 
-    <el-table
-      :key="tableKey"
-      v-loading="listLoading"
-      :data="list"
-      border
-      fit
-      :header-cell-style="{background:'#ebeef5'}"
-      style="width: 100%;"
-      @sort-change="sortChange"
-    >
-      <el-table-column type="index" align="center" />
-      <el-table-column :label="$t('table.create_date')" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.create_date | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column :label="$t('table.warehouse_code')" align="center" width="100px">
-        <template slot-scope="{row}">
-          <span>{{ row.warehouse_code }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column :label="$t('table.ref_no')" align="center" width="150px">
-        <template slot-scope="{row}">
-          <span>{{ row.ref_no }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column :label="$t('table.po_no')" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.po_no }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column :label="$t('table.location_code')" align="center" width="150px">
-        <template slot-scope="{row}">
-          <span>{{ row.location_code }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column :label="$t('table.sku_no')" align="center">
-        <template slot-scope="{row}">
-          <span>{{ row.sku_no }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column :label="$t('table.drap_id')" align="center" class-name="status-col">
-        <template slot-scope="{row}">
-          <span>{{ row.drap_id }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column :label="$t('table.qty')" align="center" width="100px" class-name="small-padding fixed-width">
-        <template slot-scope="{row}">
-          <span>{{ row.qty }}</span>
-        </template>
-      </el-table-column>
-    </el-table>
+    <el-card class="box-card" style="margin-top: 20px;">
+      <el-table
+        :key="tableKey"
+        v-loading="listLoading"
+        :data="list"
+        border
+        fit
+        :header-cell-style="{background:'#ebeef5'}"
+        style="width: 100%;"
+        @sort-change="sortChange"
+      >
+        <el-table-column type="index" align="center" />
+        <el-table-column :label="$t('table.create_date')" align="center">
+          <template slot-scope="{row}">
+            <span>{{ row.create_date | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column :label="$t('table.warehouse_code')" align="center" width="100px">
+          <template slot-scope="{row}">
+            <span>{{ row.warehouse_code }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column :label="$t('table.ref_no')" align="center" width="150px">
+          <template slot-scope="{row}">
+            <span>{{ row.ref_no }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column :label="$t('table.po_no')" align="center">
+          <template slot-scope="{row}">
+            <span>{{ row.po_no }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column :label="$t('table.location_code')" align="center" width="150px">
+          <template slot-scope="{row}">
+            <span>{{ row.location_code }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column :label="$t('table.sku_no')" align="center">
+          <template slot-scope="{row}">
+            <span>{{ row.sku_no }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column :label="$t('table.drap_id')" align="center" class-name="status-col">
+          <template slot-scope="{row}">
+            <span>{{ row.drap_id }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column :label="$t('table.qty')" align="center" width="100px" class-name="small-padding fixed-width">
+          <template slot-scope="{row}">
+            <span>{{ row.qty }}</span>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
 
     <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.page_limit" @pagination="getList" />
   </div>
