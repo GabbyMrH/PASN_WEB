29cf840224f4ae48449a8f556502cf89c2ef108f
GabbyMrH <932585440@qq.com>
2020-05-22 00:18:20 +0800

    修改样式i

--- WEB_APP/src/views/order/inbound/index.vue
+++ WEB_APP/src/views/order/inbound/index.vue
@@ -1,99 +1,103 @@
 <template>
   <div class="app-container">
-    <div class="filter-container">
-      <el-input v-model="listQuery.ref_no" clearable :placeholder="$t('table.ref_no')" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-      <el-input v-model="listQuery.booking_no" clearable :placeholder="$t('table.booking_no')" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
-      <el-date-picker
-        v-model="pickerDate"
-        class="filter-item"
-        type="daterange"
-        align="right"
-        unlink-panels
-        range-separator="至"
-        start-placeholder="开始日期"
-        end-placeholder="结束日期"
-        value-format="yyyy-MM-dd"
-        :picker-options="pickerOptions"
-        @change="pickerDateValue"
-      />
-      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
-        {{ $t('table.search') }}
-      </el-button>
-    </div>
+    <el-card class="box-card">
+      <div class="filter-container">
+        <el-input v-model="listQuery.ref_no" clearable :placeholder="$t('table.ref_no')" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+        <el-input v-model="listQuery.booking_no" clearable :placeholder="$t('table.booking_no')" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+        <el-date-picker
+          v-model="pickerDate"
+          class="filter-item"
+          type="daterange"
+          align="right"
+          unlink-panels
+          range-separator="至"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+          value-format="yyyy-MM-dd"
+          :picker-options="pickerOptions"
+          @change="pickerDateValue"
+        />
+        <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+          {{ $t('table.search') }}
+        </el-button>
+      </div>
+    </el-card>
 
-    <el-row class="table-style">
-      <el-col class="table-button">
-        <router-link :to="{ name:'InBoundCreateOrUpdate', params: createOrUpdateData }">
-          <el-button v-waves plain icon="el-icon-plus" size="small" @click="handleCreate()">创建订单</el-button>
-        </router-link>
-        <!-- <el-button v-waves plain type="primary" icon="el-icon-edit" size="small">编辑</el-button> -->
-        <!-- <el-button v-waves plain type="danger" icon="el-icon-delete" size="small">批量删除</el-button> -->
-        <!-- <el-button v-waves plain type="success" icon="el-icon-tickets" size="small">收货报告</el-button> -->
-      </el-col>
-      <el-col>
-        <el-table
-          :key="tableKey"
-          v-loading="listLoading"
-          :data="list"
-          border
-          fit
-          highlight-current-row
-          :header-cell-style="{background:'#ebeef5'}"
-          style="width: 100%;"
-        >
-          <el-table-column
-            type="index"
-            width="55"
-            align="center"
-            label="序号"
-          />
-          <el-table-column :label="$t('table.ref_no')" prop="ref_no" align="center" />
-          <el-table-column :label="$t('table.booking_no')" prop="booking_no" align="center" />
-          <el-table-column :label="$t('table.booking_date')" align="center" prop="booking_data" sortable>
-            <template slot-scope="{row}">
-              <span>{{ row.booking_date | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column :label="$t('table.booking_status')" align="center" prop="booking_status">
-            <template slot-scope="{row}">
-              <el-tag :type="row.booking_status | statusFilter">
-                {{ row.booking_status }}
-              </el-tag>
-            </template>
-          </el-table-column>
-          <el-table-column :label="$t('table.order_qty')" align="center" prop="order_qty" sortable />
-          <el-table-column label="收货日期" align="center" prop="booking_date" sortable>
-            <template slot-scope="{row}">
-              <span>{{ row.booking_date | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
-            </template>
-          </el-table-column>
-          <el-table-column :label="$t('table.customer_id')" align="center" prop="customer_id" />
-          <el-table-column :label="$t('table.warehouse_code')" align="center" prop="warehouse_code" />
-          <el-table-column :label="$t('table.otherActions')" align="center">
-            <template>
-              <el-tooltip effect="dark" content="收货报告" placement="top">
-                <el-button icon="el-icon-tickets" size="mini" round />
-              </el-tooltip>
-              <el-tooltip effect="dark" content="QC下载" placement="top">
-                <el-button icon="el-icon-picture-outline" size="mini" round />
-              </el-tooltip>
-            </template>
-          </el-table-column>
-          <el-table-column :label="$t('table.actions')" align="center" width="160" class-name="small-padding fixed-width">
-            <template slot-scope="{row}">
-              <router-link :to="{ name:'InBoundCreateOrUpdate', params: createOrUpdateData }">
-                <el-button v-if="row.booking_status == 'NEW' " type="primary" size="mini" @click="handleUpdate(row)">
-                  {{ $t('table.edit') }}
+    <el-card class="box-card" style="margin-top: 20px;">
+      <el-row class="table-style">
+        <el-col class="table-button">
+          <router-link :to="{ name:'InBoundCreateOrUpdate', params: createOrUpdateData }">
+            <el-button v-waves plain icon="el-icon-plus" size="small" @click="handleCreate()">创建订单</el-button>
+          </router-link>
+          <!-- <el-button v-waves plain type="primary" icon="el-icon-edit" size="small">编辑</el-button> -->
+          <!-- <el-button v-waves plain type="danger" icon="el-icon-delete" size="small">批量删除</el-button> -->
+          <!-- <el-button v-waves plain type="success" icon="el-icon-tickets" size="small">收货报告</el-button> -->
+        </el-col>
+        <el-col>
+          <el-table
+            :key="tableKey"
+            v-loading="listLoading"
+            :data="list"
+            border
+            fit
+            highlight-current-row
+            :header-cell-style="{background:'#ebeef5'}"
+            style="width: 100%;"
+          >
+            <el-table-column
+              type="index"
+              width="55"
+              align="center"
+              label="序号"
+            />
+            <el-table-column :label="$t('table.ref_no')" prop="ref_no" align="center" />
+            <el-table-column :label="$t('table.booking_no')" prop="booking_no" align="center" />
+            <el-table-column :label="$t('table.booking_date')" align="center" prop="booking_data" sortable>
+              <template slot-scope="{row}">
+                <span>{{ row.booking_date | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column :label="$t('table.booking_status')" align="center" prop="booking_status">
+              <template slot-scope="{row}">
+                <el-tag :type="row.booking_status | statusFilter">
+                  {{ row.booking_status }}
+                </el-tag>
+              </template>
+            </el-table-column>
+            <el-table-column :label="$t('table.order_qty')" align="center" prop="order_qty" sortable />
+            <el-table-column label="收货日期" align="center" prop="booking_date" sortable>
+              <template slot-scope="{row}">
+                <span>{{ row.booking_date | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+              </template>
+            </el-table-column>
+            <el-table-column :label="$t('table.customer_id')" align="center" prop="customer_id" />
+            <el-table-column :label="$t('table.warehouse_code')" align="center" prop="warehouse_code" />
+            <el-table-column :label="$t('table.otherActions')" align="center">
+              <template>
+                <el-tooltip effect="dark" content="收货报告" placement="top">
+                  <el-button icon="el-icon-tickets" size="mini" round />
+                </el-tooltip>
+                <el-tooltip effect="dark" content="QC下载" placement="top">
+                  <el-button icon="el-icon-picture-outline" size="mini" round />
+                </el-tooltip>
+              </template>
+            </el-table-column>
+            <el-table-column :label="$t('table.actions')" align="center" width="160" class-name="small-padding fixed-width">
+              <template slot-scope="{row}">
+                <router-link :to="{ name:'InBoundCreateOrUpdate', params: createOrUpdateData }">
+                  <el-button v-if="row.booking_status == 'NEW' " type="primary" size="mini" @click="handleUpdate(row)">
+                    {{ $t('table.edit') }}
+                  </el-button>
+                </router-link>
+                <el-button v-if="row.booking_status=='RECEIPTING'" size="mini" type="danger" @click="handleDelete(row)">
+                  {{ $t('table.delete') }}
                 </el-button>
-              </router-link>
-              <el-button v-if="row.booking_status=='RECEIPTING'" size="mini" type="danger" @click="handleDelete(row)">
-                {{ $t('table.delete') }}
-              </el-button>
-            </template>
-          </el-table-column>
-        </el-table>
-      </el-col>
-    </el-row>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-col>
+      </el-row>
+    </el-card>
 
     <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.page_limit" @pagination="getList" />
   </div>
